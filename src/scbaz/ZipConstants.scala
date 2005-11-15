655d9b9e2936739691dc0232d8495907d19fb0f2
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-15 16:38:32 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5183 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ src/scbaz/ZipConstants.scala
@@ -0,0 +1,8 @@
+package java.util.zip ;
+
+
+// XXX this is present only because I can't get my stuff compiling
+// otherwise.  Indeed, ZipConstants appears to be missing
+// from my rt.jar .  Is this an error in my rt.jar, or
+// should Scala not be looking for this file?
+trait ZipConstants { }
