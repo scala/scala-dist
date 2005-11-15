fa79b5ad7cc9bb2d544b434b3cac2fe9b8f5a57b
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-15 17:10:37 +0000

    rudimentary implementation of package installation
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5184 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/scbaz/ManagedDirectory.scala
+++ src/scbaz/ManagedDirectory.scala
@@ -1,16 +1,131 @@
 package scbaz;
 
-import scala.collection.mutable.Set ;
-import scala.collection.mutable.HashSet ;
+import java.io.{File, FileReader, FileOutputStream, BufferedOutputStream} ;
+import scala.collection.immutable._ ;
+import scala.xml._ ;
+import java.util.zip.{ZipFile,ZipEntry} ;
 
 class ManagedDirectory(directory : java.io.File) {
+  val scbaz_dir = new File(directory, "scbaz") ;
+  // XXX what if the scbaz directory doesn't exist?
+  // option 1: have it automatically throw an error here
+  // option 2: have a verify() method
+  // option 3: have a ensureExists() method
+  // multiple options possible; 2+3 looks reasonable
 
-//  var universe : Universe;
+  var universe : Universe = new EmptyUniverse() ;
 
-  val installed : Set[Package]  =  new HashSet();
-  val available : Set[Package] = new HashSet();
+  var available : PackageSet = PackageSet.Empty ;
+  val installed : PackageSet  =  PackageSet.Empty ;
 
-  def setAvailable() = {  }
+  def loadAvailable() = {
+    val file = new File(scbaz_dir, "available") ;
 
-  def install(pack : Package) = {  }
+    if(file.exists()) {
+      val node = XML.load(file.getAbsolutePath()) ;
+      available = PackageSet.fromXML(node) ;
+    }
+  }
+  val _ = loadAvailable()  ;
+
+
+  val downloader = new Downloader(new File(scbaz_dir, "cache")) ;
+
+
+
+  def install(pack : Package) = { 
+    // parse a zip-ish "/"-delimited filename into a relative File
+    def zipToFile(name:String) : File = {
+      val path_parts = name.split("/").toList.filter(s => s.length() > 0) ;
+      val file = path_parts.foldLeft (new File(""))
+	                             ((d,n) => new File(d,n)) ;
+      file
+    }
+
+    // turn an Enumeration into a List
+    def mkList[A](enum:java.util.Enumeration) : List[A] = {
+      var l : List[A] = Nil ;
+      while(enum.hasMoreElements()) {
+	val n = enum.nextElement().asInstanceOf[A] ;
+	l = n :: l ;
+      }
+
+      l.reverse
+    }
+
+    // XXX should uninstall same-named package if there is one
+
+    if(! downloader.is_downloaded(pack.filename)) {
+      downloader.download(pack.link, pack.filename)
+    }
+
+    val zip = new ZipFile(new File(downloader.dir, pack.filename)) ;
+
+    val installedFiles = mkList[ZipEntry](zip.entries())
+                         .map(ent => zipToFile(ent.getName()));
+
+    val newEntry = new InstalledEntry(pack.name, pack.version,
+				      installedFiles,
+				      false) ;
+
+    // XXX comment in when these functions are available
+    // installed.add(newEntry)
+    // saveInstalled()
+    
+
+    for(val ent <- mkList[ZipEntry](zip.entries())) {
+      val file = new File(directory, zipToFile(ent.getName()).getPath()) ;
+      if(ent.isDirectory()) {
+	file.mkdirs();
+      } else {
+	val in = zip.getInputStream(ent) ;
+	val out = new BufferedOutputStream(new FileOutputStream(file));
+	
+	  
+	val buf = new Array[byte](1024);
+	def lp() : Unit = {
+	  val len = in.read(buf) ;
+	  if(len >= 0) {
+	    out.write(buf, 0, len);
+	    lp();
+	  }
+	}
+	lp();
+
+	in.close();
+	out.close();
+      }
+    }
+
+    // XXX comment in when these functions are available
+    // installed.add(newEntry.completed)
+    // saveInstalled
+  }
+
+
+  override def toString() = {
+    "(" + directory.toString() + ": " +
+        installed.packages.length + "/" +
+        available.packages.length + " packages)"
+  }
+
+}
+
+
+
+object TestManagedDirectory {
+  def main(args:Array[String]) = {
+    val dir = new ManagedDirectory(new File("/home/lex/scala/scbaz/hacks/testcli"));
+
+    Console.println(dir);
+
+    dir.available.newestNamed("scbaz") match {
+      case None => {
+	Console.println("no scbaz package available!"); }
+      case Some(pack) => {
+	Console.println("installing " + pack);
+	dir.install(pack);
+      }
+    }
+  }
 }
