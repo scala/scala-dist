41e8b1842fc9a2b5efeec177140ea0e27bd4f2f2
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-22 11:48:20 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5213 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ src/scbaz/messages/AddPackage.scala
@@ -0,0 +1,19 @@
+package scbaz.messages;
+
+import scala.xml._;
+
+case class AddPackage(pack:Package)
+extends Message {
+  override def toXML = 
+    Elem(null, "addpackage", Null, TopScope,
+	 pack.toXML)
+
+}
+
+
+object AddPackageUtil {
+  def fromXML(node:Node) = {
+    val pack = Package.fromXML((node \ "package")(0));
+    new AddPackage(pack);
+  }
+}
