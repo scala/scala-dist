faa0ab998d2648c730e870dc1d8ba4776bd24e66
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-22 11:37:07 +0000

    - fixed definition of toXML - added simpleUniverses method
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5202 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/scbaz/EmptyUniverse.scala
+++ src/scbaz/EmptyUniverse.scala
@@ -6,5 +6,6 @@ class EmptyUniverse extends Universe("The Empty Universe",
 				     "A universe with no packages.")
 {
   def retrieveAvailable() = new PackageSet(Nil) ;
-  def toXML = Elem(null, "emptyuniverse", Null, TopScope) ;
+  override def simpleUniverses = List() ;
+  override def toXML = Elem(null, "emptyuniverse", Null, TopScope) ;
 }
