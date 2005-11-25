de5cf1c7ffe0494e538e0fe58822a8605330cedb
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-25 16:58:50 +0000

    - changed to using a URL instead of hostname, port, and packagesURL - added requestFromServer() - updated retrieveAvailable() in light of the above
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5247 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/SimpleUniverse.scala
+++ src/SimpleUniverse.scala
@@ -3,45 +3,56 @@ package scbaz;
 import java.net.URL ;
 import java.io._ ;
 import scala.xml._ ;
+import scbaz.messages._ ;
 
 // A SimpleUniverse is a kind of universe that downloads packages
 // only from one server.  The packages in the universe are then
 // exactly the packages supplied by the one server.
 class SimpleUniverse(name0:String, description0:String,
-                     val hostname:String, val port:Int,
-		     val packagesURL : URL)
+		     val location: URL)
 extends Universe(name0,description0) {
-  def retrieveAvailable() = {
-    val connection = packagesURL.openConnection();
-    val inputStream = connection.getInputStream();
-    val outputStream = new ByteArrayOutputStream();
-
-    def lp():Unit = {
-      val dat = new Array[byte](1000);
-      val numread = inputStream.read(dat);
-      if(numread >= 0) {
-	outputStream.write(dat,0,numread);
-	lp();
-      }
+  override def retrieveAvailable(): PackageSet = {
+    val response = requestFromServer(SendPackageList());
+    response match {
+      case LatestPackages(packs) => packs;
+      case _ => throw new Error("unexpected response: " + response); // XXX choose a more specific exception
     }
-    lp();
-    
-    // XXX this should consider character encodings more carefully....
-    //     the local default is not a reliable choice.
-    val packagesText = outputStream.toString();
-
-    val xmlNode = XML.load(new StringReader(packagesText));
-    val packageNodes = (xmlNode \ "package").toList ;
-
-    val packages = packageNodes.map(node => Package.fromXML(node));
-
-    new PackageSet(packages)
   }
 
   override def simpleUniverses = List(this) ;
 
+  // Make a low-level request to the server.
+  // This method blocks until the server responds.
+  // It throws an exception if there is a network problem.
+  // XXX learn and document which exceptions are thrown
+  def requestFromServer(request: Message): Message = {
+    val connection = location.openConnection();
+    connection.setDoOutput(true);
+    val out = connection.getOutputStream();
+    val bytesOut = request.toXML.toString().getBytes("UTF-8");
+    out.write(bytesOut);
+    out.close();
+
+    val in = connection.getInputStream();
+    val respBuf = new ByteArrayOutputStream();
+    def lp():Unit = {
+      val dat = new Array[byte](1000);
+      val n = in.read(dat);
+      if(n >= 0) {
+	respBuf.write(dat,0,n);
+	lp();
+      }
+    }
+    lp();
+   
+    // XXX this should use whatever encoding the server specifified,
+    // not hard code it to UTF-8
+    val respString = respBuf.toString("UTF-8");
+    MessageUtil.fromXML(XML.load(new StringReader(respString)));
+  }
+
   override def toString() = 
-    "Universe \"" + name + "\" (" + hostname + ":" + port + ")";
+    "Universe \"" + name + "\" (" + location + ")";
 
   def toXML = 
 <simpleuniverse>
@@ -49,25 +60,20 @@ extends Universe(name0,description0) {
   <description>
     {description}
   </description>
-  <hostname>{hostname}</hostname>
-  <port>{port}</port>
-  <packagesURL>{packagesURL}</packagesURL>
+  <location>{location}</location>
 </simpleuniverse> ;
 }
 
 
-object SimpleUniverse {
+// XXX naming it SimpleUniverse causes a compiler crash
+object SimpleUniverseUtil {
   def fromXML(node:Node) = {
-    val name = (node \ "name")(0).child(0).toString(false);
-    val description = (node \ "description")(0).child(0).toString(false);
-    val hostname = (node \ "hostname")(0).child(0).toString(false);
-    val portString = (node \ "port")(0).child(0).toString(false);
-    val urlString = (node \ "packagesURL")(0).child(0).toString(false);
+    val name = (node \ "name")(0).child(0).text;
+    val description = (node \ "description")(0).child(0).text;
+    val linkString = (node \ "location")(0).child(0).text;
+    val link = new URL(linkString);
 
-    val port = Integer.decode(portString).intValue();
-    val url = new URL(urlString);
-
-    new SimpleUniverse(name, description, hostname, port, url);
+    new SimpleUniverse(name, description, link);
   }
 }
 
@@ -76,9 +82,7 @@ object TestSimpleUniverse {
   def main(args:Array[String]):Unit = {
     val univ = new SimpleUniverse("scala-dev",
 				  "development universe of Scala",
-				  "scalauniverses.dnsalias.net",
-				  23256,
-				  new URL("http://localhost/~lex/expacks/packages"));
+				  new URL("http://scalauniverses.dnsalias.net:23256/scala-dev"));
 
     val xml = univ.toXML ;
 
