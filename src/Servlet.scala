b0ac1fb53c7e32838400a2fdc7d53e83c3091b5b
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-25 15:56:43 +0000

    basic version in place
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5236 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/Servlet.scala
+++ src/Servlet.scala
@@ -1,8 +1,9 @@
 package scbaz;
 
-import java.io._;
-import javax.servlet.http._;
-import javax.servlet._;
+import java.io._ ;
+import javax.servlet.http._ ;
+import javax.servlet._ ;
+import scala.xml._ ;
 
 // An Servlet interface for updating a simple universe that is
 // stored in a directory.  This class merely converts between
@@ -11,22 +12,39 @@ import javax.servlet._;
 
 class Servlet
 extends HttpServlet {
-  var config:ServletConfig = null;
+  var config: ServletConfig = null;
   def context = config.getServletContext() ;
 
   override def init(config:ServletConfig) =  {
     super.init(config);
     this.config = config;
+
   }
 
-  override def doGet (req:HttpServletRequest,
-		      res:HttpServletResponse) =
-  {
-    val reqXML = ...
-    val handler = ...
-    val respXML = handler.handleRequest(handler)
+  def handler: ServletRequestHandler = {
+    // for some reason, the following computation does not work
+    // when called from init().
+    val dirname = context.getInitParameter("dirname").asInstanceOf[String];
+    ServletRequestHandler.handlerFor(dirname);
+  }
 
+  def universe: Universe = handler.universe ;
+
+
+  override def doPost (req:HttpServletRequest,
+		       res:HttpServletResponse) =  
+  {
+    val reqXML = XML.load(req.getReader());
+    val respXML = handler.handleRequest(reqXML);
+    res.getWriter().write(respXML.toString());
+  }
+
+  // XXX GET's should also defer to the handler; move the code over there...
+  override def doGet (req:HttpServletRequest,
+		      res:HttpServletResponse) =  
+  {
     val out = res.getWriter();
-    out.print(respXML.toString());
+    out.println("This is a Scala Bazaar.  The bazaar descriptor is:");
+    out.println(universe.toXML.toString());
   }
 }
