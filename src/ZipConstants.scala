66c82402704d6b7b53a956dc6874ed59eef6939d
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-25 17:38:16 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5257 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/ZipConstants.scala
+++ src/ZipConstants.scala
@@ -3,8 +3,11 @@ package java.util.zip ;
 
 // XXX this is present only because I can't get my stuff compiling
 // otherwise.  Indeed, ZipConstants appears to be missing
-// from my rt.jar .  Is this an error in my rt.jar, or
-// should Scala not be looking for this file?
+// from my rt.jar .  It looks like the compiler ought to look
+// at a development .jar instead of rt.jar. Probably ZipConstants
+// is included in the former but not the latter.
+// (Although, maybe rt.jar should include it anyway, in case of
+// compilers that don't optimize away accesses to final static int's)
 trait ZipConstants { }
 
 
