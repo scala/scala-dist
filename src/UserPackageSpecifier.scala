208a65694ba558f7a189a631c4b06474ccf78742
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-29 19:51:12 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5271 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ src/UserPackageSpecifier.scala
@@ -0,0 +1,42 @@
+package scbaz;
+
+// a specification from a user for a package within a universe
+abstract class UserPackageSpecifier {
+  def chooseFrom(packages: PackageSet): Option[Package];
+}
+
+
+// a request for the newest package of a given name
+case class UPSNewestNamed(val name: String) 
+extends UserPackageSpecifier {
+  def chooseFrom(packages: PackageSet): Option[Package] =
+    packages.newestNamed(name);
+
+  override def toString() = name;
+}
+
+// a request for a package with a specific version
+case class UPSWithSpec(val spec: PackageSpec)
+extends UserPackageSpecifier {
+  def chooseFrom(packages: PackageSet): Option[Package] =
+    packages.packageWithSpec(spec);
+
+  override def toString() = spec.toString()
+}
+
+
+object UserPackageSpecifierUtil {
+  // Parse a specifier from a user-supplied string.
+  // If the string is simply a name, then a UPSNewestNamed is returned.
+  // If the string includes a '/' character, then it is assumed
+  // to be a PackageSpecification in slash notation, so a UPSWithSpec
+  // is returned.
+  def fromString(str: String): UserPackageSpecifier = {
+    if(str.indexOf("/") >= 0) {
+      val spec = PackageSpecUtil.fromSlashNotation(str);
+      UPSWithSpec(spec);
+    } else {
+      UPSNewestNamed(str);
+    }
+  }
+}
