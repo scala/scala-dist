64a8fcc3e5bd88ae006abea6800857b62b63d6f7
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-11-29 19:00:25 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5268 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/messages/AddPackage.scala
+++ src/messages/AddPackage.scala
@@ -13,7 +13,7 @@ extends Message {
 
 object AddPackageUtil {
   def fromXML(node:Node) = {
-    val pack = Package.fromXML((node \ "package")(0));
+    val pack = PackageUtil.fromXML((node \ "package")(0));
     new AddPackage(pack);
   }
 }
