a9c0421a7d6c98a6037e6a9cb2a6dcb6d290c8b1
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-12-02 11:27:20 +0000

    tweaked the comment
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5292 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/ZipConstants.scala
+++ src/ZipConstants.scala
@@ -1,13 +1,10 @@
 package java.util.zip ;
 
 
-// XXX this is present only because I can't get my stuff compiling
-// otherwise.  Indeed, ZipConstants appears to be missing
-// from my rt.jar .  It looks like the compiler ought to look
-// at a development .jar instead of rt.jar. Probably ZipConstants
-// is included in the former but not the latter.
-// (Although, maybe rt.jar should include it anyway, in case of
-// compilers that don't optimize away accesses to final static int's)
+// XXX This is present only to make the compiler happy. ZipConstants
+// appears to be missing from rt.jar in Sun's SDK for1.4 .  It looks
+// like the compiler ought to look at a development .jar instead of
+// rt.jar; presumably, ZipConstants is present there.
 trait ZipConstants { }
 
 
