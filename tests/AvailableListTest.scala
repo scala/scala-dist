924795415fd8ae5b6996a269b9674e69bd2987d4
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-12-05 20:12:54 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5299 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ tests/AvailableListTest.scala
@@ -0,0 +1,39 @@
+package sbaz;
+
+import junit.framework._ ;
+import java.net.URL;
+
+class AvailableListTest extends TestCase {
+  def testAltXML = {
+    val xml =
+      <packageset>
+        <package>
+            <name>foo</name>
+            <version>1.0</version>
+            <depends/>
+            <description>an example package</description>
+            <link>http://www.foo.org/downloads/foo-1.0.zip</link>
+	</package>
+        <package>
+            <name>foo</name>
+            <version>1.2</version>
+            <depends/>
+            <description>an example package</description>
+            <link>http://www.foo.org/downloads/foo-1.2.zip</link>
+	</package>
+      </packageset>;
+
+    val decoded = AvailableListUtil.fromXML(xml);
+    val Some(entry) = decoded.newestNamed("foo");
+
+    Assert.assertTrue(entry.pack.name == "foo");
+    Assert.assertTrue(entry.pack.version == new Version("1.2"));
+    Assert.assertTrue(entry.pack.depends.isEmpty);
+    Assert.assertTrue(entry.pack.description.startsWith("an example"));
+    Assert.assertTrue(entry.link ==
+		      new URL("http://www.foo.org/downloads/foo-1.2.zip"));
+
+  }
+
+}
+
