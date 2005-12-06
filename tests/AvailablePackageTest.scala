1b68931349b368917fd92684111118a35f188f65
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-12-06 09:42:02 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5305 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- tests/AvailablePackageTest.scala
+++ tests/AvailablePackageTest.scala
@@ -2,6 +2,7 @@ package sbaz;
 
 import junit.framework._ ;
 import java.net.URL;
+import scala.collection.immutable.ListSet;
 
 class AvailablePackageTest extends TestCase {
   def testAltXML = {
@@ -25,5 +26,21 @@ class AvailablePackageTest extends TestCase {
 
   }
 
+  def testXMLandBack = {
+    val pack =
+      new AvailablePackage(
+	new Package("foo",
+		    new Version("1.0"),
+		    new URL("http://www.foo.org/downloads/foo-1.0.zip"),
+		    new ListSet(),
+		    "an example package"),
+	new URL("http://www.foo.org/downloads/foo-1.0.zip"));
+
+    val xml = pack.toXML;
+    val pack2 = AvailablePackageUtil.fromXML(xml);
+
+    Assert.assertTrue(pack.spec == pack2.spec);
+    Assert.assertTrue(pack.link == pack2.link);
+  }
 }
 
