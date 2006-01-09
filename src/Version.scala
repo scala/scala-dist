1e902c472a996b462e3288b75ac40fe95464c5a9
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-01-09 15:12:08 +0000

    Changed the ordering of versions.  Instead of simple string comparison, they are now divided into a sequence of components (alphabetic, numeric, symbolic) which are then compared lexigraphically.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5473 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/Version.scala
+++ src/Version.scala
@@ -1,29 +1,145 @@
 package sbaz;
 
-class Version(val str:String) {
-  // simple placeholder for versions.  Eventually it will parse
-  // the string into a sequence of strings and compare with
-  // lexigraphic ordering.  perhaps the Debian version scheme
-  // could be copied directly....
+// A component of a Version.  A Version number is divided into
+// a sequence of components.  Each component is either a sequence
+// of letters, a sequence of numbers, or a sequence of symbols.
+// Versions are compared lexigraphically by their list of components.
+sealed class VersionComp;
+case class VCAlpha(val str: String) extends VersionComp {
+  override def toString() = str
+}
+case class VCNum(val num: int) extends VersionComp {
+  override def toString(): String = num.toString()
+}
+case class VCSym(val syms: String) extends VersionComp {
+  override def toString() = syms
+}
 
-  override def toString() = str;
+class Version(val comps: List[VersionComp]) extends Ordered[Version] {
+  def this(str: String) = this(VersionUtil.componentsFrom(str))
 
-  // XXX is there a Comparable mixin?
-  def < (v : Version) = {
-    str.compareTo(v.toString()) < 0
+  override def toString(): String = {
+    val buf = new StringBuffer();
+    for(val vc <- comps) {
+      buf.append(vc)
+    }
+    buf.toString()
   }
 
-  def > (v : Version) = {
-    str.compareTo(v.toString()) > 0
-  }
 
-  def >= (v : Version) = {
-    str.compareTo(v.toString()) >= 0
-  }
-
-
-  override def equals (v : Any) = {
+  override def equals (v : Any): Boolean = {
     (v.isInstanceOf[Version] &&
-     str == v.asInstanceOf[Version].str)
+     (compareTo(v.asInstanceOf[Version]) == 0))
+  }
+
+  override def compareTo[b >: Version <% Ordered[b]](that: b): int =
+    that match {
+      case that: Version => {
+	def cmpComps(comps1: List[VersionComp],
+		     comps2: List[VersionComp]): int = 
+	  comps1 match {
+	    case Nil =>
+	      comps2 match {
+		case Nil => 0;
+		case _ => -1;
+	      }
+	    case c1::rest1 =>
+	      comps2 match {
+		case Nil => 1;
+		case c2::rest2 =>
+		  VersionUtil.compareComps(c1, c2) match {
+		    case 0 => cmpComps(rest1, rest2)
+		    case ord => ord
+		  }
+	      }
+	  }
+
+	cmpComps(comps, that.comps)
+      }
+
+      case _ => -(that compareTo this)
+    }
+}
+
+
+
+object VersionUtil {
+  // Compare two version components.  Alpha's come first,
+  // followed by numbers, followed by symbols.  The return
+  // value is as for Ordered.compareTo() .
+  def compareComps(c1: VersionComp, c2: VersionComp): int = {
+    Pair(c1, c2) match {
+      case Pair(VCAlpha(s1), VCAlpha(s2)) =>
+	s1.compareTo(s2)
+      case Pair(VCNum(n1), VCNum(n2)) =>
+	if(n1 < n2) -1 else
+	  if(n1 > n2) 1 else 0
+      case Pair(VCSym(s1), VCSym(s2)) =>
+	s1.compareTo(s2)
+      case Pair(VCAlpha(_), VCNum(_)) => -1
+      case Pair(VCAlpha(_), VCSym(_)) => -1
+      case Pair(VCNum(_), VCAlpha(_)) => 1
+      case Pair(VCNum(_), VCSym(_)) => -1
+      case Pair(VCSym(_), VCAlpha(_)) => 1
+      case Pair(VCSym(_), VCNum(_)) => 1  // XXX apparent compiler
+                                          // bug if I do _:VCSym instead
+                                          // of VCSym(_)
+    }
+  }
+
+
+  // parse a version string into a list of version components
+  def componentsFrom(str: String) = {
+    def ctype(c: char) = 
+      if(Character.isLetter(c))
+	'alpha
+      else if((c >= '0') && (c <= '9'))
+	'num
+      else
+	'sym
+
+    // the growing list of components, in reverse
+    var rcomps: List[VersionComp] = Nil;
+
+
+    // add a component from the given range in the string
+    def addvc(start: int, end: int) = {
+      val substr = str.substring(start, end+1)
+      val vc = ctype(substr.charAt(0)) match {
+	case 'num => VCNum(Integer.parseInt(substr))
+	case 'alpha => VCAlpha(substr)
+	case 'sym => VCSym(substr)
+      }
+      rcomps = vc :: rcomps;
+    }
+
+    // The following loop moves a window (start, end) through
+    // str.  At the entry, the substring start..end is assumed
+    // to all have the same character types.  Extend 'end' if
+    // possible.  If not, register a new component and then
+    // start on a new one.
+    def lp(start: int, end: int): Unit = {
+      if(end+1 >= str.length()) {
+	// reached the end of the string
+	if(end >= start)
+	  addvc(start,end)
+      }
+      else {
+	if(start > end)
+	  lp(start, end+1)  // starting a new component
+	else {
+	  if(ctype(str.charAt(start)) == ctype(str.charAt(end+1)))
+	    lp(start, end+1)  // same ctype -- extend the comp 1 more
+	  else {
+	    // different ctype; start a new component
+	    addvc(start, end) 
+	    lp(end+1, end)
+	  }
+	}
+      }
+    }
+    lp(0, -1)
+
+    rcomps.reverse
   }
 }
