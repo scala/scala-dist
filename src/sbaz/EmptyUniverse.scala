bbb409aed25356a5ed32088a8b6ed20d086f76ae
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-02-02 11:20:00 +0000

    tiny cleanups
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5680 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/sbaz/EmptyUniverse.scala
+++ src/sbaz/EmptyUniverse.scala
@@ -2,10 +2,10 @@ package sbaz ;
 
 import scala.xml._ ;
 
-class EmptyUniverse extends Universe("The Empty Universe",
+class EmptyUniverse extends Universe("empty",
 				     "A universe with no packages.")
 {
   def retrieveAvailable() = new AvailableList(Nil) ;
   override def simpleUniverses = List() ;
-  override def toXML = Elem(null, "emptyuniverse", Null, TopScope) ;
+  override def toXML = <emptyuniverse/> ;
 }
