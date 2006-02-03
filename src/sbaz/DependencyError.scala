0b7ea597fd5269a07e5b8df4d360c9a1fa925590
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-02-03 12:17:33 +0000

    added an optional explanation string
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5699 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/sbaz/DependencyError.scala
+++ src/sbaz/DependencyError.scala
@@ -2,6 +2,8 @@ package sbaz;
 
 // An error in dependency management.  These are most frequently
 // thrown by the routines of AvailableList.
-class DependencyError()
-extends Error;
+class DependencyError(explanation: String)
+extends Error(explanation) {
+  def this() = this("dependency error")
+}
 
