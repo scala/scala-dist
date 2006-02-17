b01d89a33f0d0e78fed9803b795253aa012b9324
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-02-17 16:46:45 +0000

    expanded "Addition" into two cases: AdditionFromFile and AdditionFromNet
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@5778 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/sbaz/ProposedChanges.scala
+++ src/sbaz/ProposedChanges.scala
@@ -1,12 +1,40 @@
 package sbaz
+import java.io.File
+import java.util.zip.ZipFile
+import scala.xml._
 
 object ProposedChanges {
   abstract sealed class ProposedChange {
     def apply(set: PackageSet): PackageSet
   }
-  case class Addition(pack: Package) extends ProposedChange {
+  
+  abstract class Addition extends ProposedChange {
+    def pack: Package
+
     def apply(set: PackageSet) =
       set + pack
+  }
+  
+  case class AdditionFromFile(file: File) extends Addition {
+    def pack = {
+      val zip = new ZipFile(file)
+      val ent = zip.getEntry("meta/description")
+      if(ent == null)
+        throw new Error("malformed package file: meta/description is missing")
+    
+
+      val inBytes = zip.getInputStream(ent)
+      val packXML = XML.load(inBytes)
+      inBytes.close()
+      zip.close()
+	
+      PackageUtil.fromXML(packXML)
+    }
+  }
+  
+  case class AdditionFromNet(avail: AvailablePackage) extends Addition {
+    def pack = avail.pack
+    def link = avail.link
   }
   
   case class Removal(pack: PackageSpec) extends ProposedChange {
