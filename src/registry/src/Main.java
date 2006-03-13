d95f52e9b42bb0f38c8453486c4756d4df91f259
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-03-13 17:59:43 +0000

    added test for undefined PATH variable when updating the Windows registry
    git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/installer/trunk@6739 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/registry/src/Main.java
+++ src/registry/src/Main.java
@@ -4,7 +4,7 @@
 // Environment: JNI Registry 3.1.3, Sun JDK 1.4.2_09
 // Version    : 01
 // Created    : 25.09.2005/mics
-// Modified   : -
+// Modified   : 13.03.2006/mics  added test for undefined PATH variable
 
 
 import com.ice.jni.registry.NoSuchKeyException;
@@ -33,15 +33,25 @@ public class Main {
             RegStringValue data = new RegStringValue(envKey, SCALA_HOME, homePath);
             envKey.setValue(data);
             // update user path
-            String path = envKey.getStringValue(PATH);
-            if (path.indexOf(SCALA_BIN) < 0) {
-            	int inx = path.lastIndexOf(";");
-            	StringBuffer buf = new StringBuffer(path);
+            String path = null;
+            try {
+                path = envKey.getStringValue(PATH);
+            }
+            catch (NoSuchValueException e) {
+                // do nothing
+            }
+            if (path == null) {
+                data = new RegStringValue(envKey, PATH, SCALA_BIN);
+                envKey.setValue(data);
+            }
+            else if (path.indexOf(SCALA_BIN) < 0) {
+                int inx = path.lastIndexOf(";");
+                StringBuffer buf = new StringBuffer(path);
                 if (0 < inx && inx < path.length()-1)
                     buf.append(";");
                 buf.append(SCALA_BIN);
                 data = new RegStringValue(envKey, PATH, buf.toString());
-                envKey.setValue(data);     
+                envKey.setValue(data);
             }
         }
         else { // uninstall
@@ -53,14 +63,16 @@ public class Main {
                 // do nothing
             }
             String path = envKey.getStringValue(PATH);
-            int inx1 = path.indexOf(SCALA_BIN);
-            if (inx1 >= 0) {
-                int inx2 = path.indexOf(";", inx1);
-                StringBuffer buf = new StringBuffer(path.substring(0, inx1));
-                if (inx2 >= 0)
-                    buf.append(path.substring(inx2));
-                RegStringValue data = new RegStringValue(envKey, PATH, buf.toString());
-                envKey.setValue(data);
+            if (path != null) {
+                int inx1 = path.indexOf(SCALA_BIN);
+                if (inx1 >= 0) {
+                    int inx2 = path.indexOf(";", inx1);
+                    StringBuffer buf = new StringBuffer(path.substring(0, inx1));
+                    if (inx2 >= 0)
+                        buf.append(path.substring(inx2));
+                    RegStringValue data = new RegStringValue(envKey, PATH, buf.toString());
+                    envKey.setValue(data);
+                }
             }
         }
 
@@ -79,17 +91,17 @@ public class Main {
         }
         else { // uninstall
             try {
-            	softwareKey.deleteSubKey(fullName);
+                softwareKey.deleteSubKey(fullName);
                 String name = fullName;
                 int inx = name.lastIndexOf("\\");
                 while (inx != -1) {
                     String parentName = name.substring(0, inx);
                     RegistryKey key = softwareKey.openSubKey(parentName, RegistryKey.ACCESS_READ);
                     int n = key.getNumberSubkeys();
-            	    if (n == 0) softwareKey.deleteSubKey(parentName);
-            	    name = parentName;
-            	    inx = name.lastIndexOf("\\");
-            	}
+                    if (n == 0) softwareKey.deleteSubKey(parentName);
+                    name = parentName;
+                    inx = name.lastIndexOf("\\");
+                }
             }
             catch (NoSuchKeyException e) {
                 // do nothing
@@ -98,16 +110,16 @@ public class Main {
     }
 
     public static void main(String[] args) {
-    	int argc = args.length;
-    	if (argc != 1 && argc != 2) {
-    	    System.out.println("Usage: java Main <version> [ <installpath> ]\n");
-    	    System.out.println("\tversion number (e.g. 1.4.0.1)");
-    	    System.out.println("\tinstall path   (e.g. c:\\\\Program Files\\\\Scala)");
-    	    System.out.println();
-    	    System.exit((argc > 0) ? 1 : 0);
-    	}
+        int argc = args.length;
+        if (argc != 1 && argc != 2) {
+            System.out.println("Usage: java Main <version> [ <installpath> ]\n");
+            System.out.println("\tversion number (e.g. 2.0.0)");
+            System.out.println("\tinstall path   (e.g. c:\\\\Program Files\\\\Scala)");
+            System.out.println();
+            System.exit((argc > 0) ? 1 : 0);
+        }
         String homePath = (argc == 2) ? args[1] : null;   // e.g. "C:\\Program Files\\Scala"
-        String fullName = "EPFL\\Scala\\" + args[0];      // e.g. "EPFL\\Scala\\1.4.0.1"
+        String fullName = "EPFL\\Scala\\" + args[0];      // e.g. "EPFL\\Scala\\2.0.0"
         try {
             updateRegistry(homePath, fullName);
         }
