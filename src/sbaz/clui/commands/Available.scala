c34ef4acd92239e45e9d15da0fb9d0fc9274fc4e
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-04-05 15:23:55 +0000

    shorten the output to cope with packages that have many versions
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@7064 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/sbaz/clui/commands/Available.scala
+++ src/sbaz/clui/commands/Available.scala
@@ -1,26 +1,69 @@
 package sbaz.clui.commands
+import scala.collection.immutable._
 
 object Available extends Command {
   val name = "available"
   val oneLineHelp = "list the available packages for installation"
   val fullHelp: String = (
-    "available\n" +
+    "available [ -a ]\n" +
     "\n" +
-    "Display the list of packages that are available for installation.\n")
+    "Display the list of packages that are available for installation.\n" +
+    "If -a is specified, then print out all versions of each package instead\n" +
+    "of just the most recent ones.")
 
 
 
   def run(args: List[String], settings: Settings) = {
     import settings._
 
-    if(! args.isEmpty)
-      usageExit
-
-    val sortedSpecs = dir.available.sortedSpecs 
-
-    for(val spec <- sortedSpecs) {
-      Console.println(spec)
+    var printall = false
+    
+    args match {
+      case Nil => ()
+      case List("-a") => printall=true
+      case _ => usageExit
     }
-    Console.println(sortedSpecs.length.toString() + " packages available")
+
+    val specs = dir.available.packages.toList.map(.spec)
+		val nameSet = specs.foldLeft(new TreeSet[String])((set,spec) => set + spec.name)
+    val names = nameSet.toList.sort((a,b) => a <= b)
+
+    def versionsFor(name: String) = {
+      val unsorted =
+        for {
+          val spec <- specs
+          spec.name == name
+        } yield spec.version
+      unsorted.sort((v1,v2) => v1 >= v2)
+    }
+      
+    def printVersions(specs: List[Version]) =
+      Console.print(specs.mkString("", ", ", ""))
+      
+    def printAllSpecs(name: String) =
+      printVersions(versionsFor(name))
+    
+    def printShortSpecs(name: String) = {
+      val toprint = 3
+      val matching = versionsFor(name)
+      if(matching.length <= toprint)
+        printVersions(matching)
+      else {
+        printVersions(matching.take(toprint))
+        Console.print(", ...")
+      }
+    }
+
+
+    for(val name <- names) {
+      Console.print(name + " (")
+      if(printall)
+        printAllSpecs(name)
+      else
+        printShortSpecs(name)
+      Console.println(")")
+    }
+    Console.println(names.length.toString + " package names")
+    Console.println(specs.length.toString + " total packages")
   }
 }
