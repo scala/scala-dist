0f9ee4d5efc82e46765510a9970856d2f5bef980
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-04-26 16:21:19 +0000

    updated (deprecated)) code
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@10854 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- tests/sbaz/ServletTest.scala
+++ tests/sbaz/ServletTest.scala
@@ -1,79 +1,81 @@
-package sbaz;
-import sbaz.messages._;
-import junit.framework._; 
-import java.net.URL;
-import scala.collection.immutable.ListSet;
+package sbaz
 
-import Assert._;
+import sbaz.messages._
+import junit.framework._
+import java.net.URL
+import scala.collection.immutable.ListSet
 
-/* Tests for the server.  It assumes that a bazaar server is 
- * available for testing.
+import Assert._
+
+/** Tests for the server.  It assumes that a bazaar server is 
+ *  available for testing.
+ *
+ *  @author Lex Spoon
  */
-
 class ServletTest extends TestCase {
   // the URL for the server to test against
-  val serverLink = new URL("http://localhost:8006/testbaz");
+  val serverLink = new URL("http://localhost:8006/testbaz")
 
   val universe = new SimpleUniverse("testbaz",
 				    "a test universe",
-				    serverLink);
+				    serverLink)
 
 
   // test that GET-ing from the URL succeeds
-  def testGET: Unit = {
-    val connection = serverLink.openConnection();
-    val in = connection.getInputStream();
+  def testGET {
+    val connection = serverLink.openConnection()
+    val in = connection.getInputStream()
 
-    def lp():Unit = {
-      val dat = new Array[byte](1000);
-      val n = in.read(dat);
-      if(n >= 0)
+    def lp() {
+      val dat = new Array[byte](1000)
+      val n = in.read(dat)
+      if (n >= 0)
 	lp();
     }
-    lp();
+    lp()
 
-    in.close();
+    in.close()
   }
 
   // test adding a package, checking it is there,
   // removing it, and testing it is gone
-  def testAddRemove: Unit = {
+  def testAddRemove {
     // a package to post
     val bogo =
       new AvailablePackage(
 	new Package(
 	      "bogo",
 	      new Version("1.0"),
-	      ListSet.Empty,
+	      ListSet.empty,
 	      "a bogus package for testing"),
 	new URL("http://www.where.ever/bogo-1.0.zip"));
 
     // post the package
     {
-      val res = universe.requestFromServer(AddPackage(bogo));
-      assertTrue(res == OK());
+      val res = universe.requestFromServer(AddPackage(bogo))
+      assertTrue(res == OK())
     }
 
     // make sure it is there
     {
-      val avail = universe.retrieveAvailable();
-      val pack = avail.packageWithSpec(bogo.spec).get;
+      val avail = universe.retrieveAvailable()
+      val pack = avail.packageWithSpec(bogo.spec).get
 
-      assertTrue(pack.spec == bogo.spec);
+      assertTrue(pack.spec == bogo.spec)
     }
 
 
     // retract the package
     {
-      val res = universe.requestFromServer(RemovePackage(bogo.spec));
-      assertTrue(res == OK());
+      val res = universe.requestFromServer(RemovePackage(bogo.spec))
+      assertTrue(res == OK())
     }
 
     // make sure the package is gone
     {
-      val avail = universe.retrieveAvailable();
-      val pack = avail.packageWithSpec(bogo.spec);
-      assertTrue(pack.isEmpty);
+      val avail = universe.retrieveAvailable()
+      val pack = avail.packageWithSpec(bogo.spec)
+      assertTrue(pack.isEmpty)
     }
   }
 
