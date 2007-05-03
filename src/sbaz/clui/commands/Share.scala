aa2765938e16676a5a00a4d82cdf715d43b28b2f
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-05-03 13:10:52 +0000

    improved error handling
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@10952 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/sbaz/clui/commands/Share.scala
+++ src/sbaz/clui/commands/Share.scala
@@ -7,72 +7,84 @@
 
 package sbaz.clui.commands
 
-import messages._
-import scala.xml.XML
 import java.io.{File, StringReader}
+import org.xml.sax.SAXParseException
+
+import scala.xml.XML
+import messages._
 
 object Share extends Command {
   val name = "share"
   val oneLineHelp = "upload a package description to the universe"
-  val fullHelp: String = (
-    "share filename\n" +
-    "share -i descriptor\n" +
-    "share --template\n" +
-    "\n" +
-    "Share a package advertisement on a bazaar.  The package advertisement\n" +
-    "is usually specified in a file, but it may also be specified on\n" +
-    "the command line with the -i option.\n" +
-    "\n" +
-    "If --template is specified, then instead of uploading a description,\n" +
-    "the command prints out a template of a package advertisement.\n")
-
+  val fullHelp: String =
+    """share filename
+    |share -i descriptor
+    |share --template
+    |
+    |Share a package advertisement on a bazaar.  The package advertisement
+    |is usually specified in a file, but it may also be specified on
+    |the command line with the -i option.
+    |
+    |If --template is specified, then instead of uploading a description,
+    |the command prints out a template of a package advertisement.
+    |""".stripMargin
 
   def run(args: List[String], settings: Settings): Unit = {
     import settings._
 
     val pack = args match {
       case List("--template") =>
-	Console.println("<availablePackage>")
-	Console.println("  <package>")
-	Console.println("    <name></name>")
-	Console.println("    <version></version>")
-	Console.println("    <depends></depends>")
-	Console.println("    <description></description>")
-	Console.println("  </package>")
-	Console.println("<link></link>")
-	Console.println("</availablePackage>")
-	null  // return() here causes a compile error
+        Console.println("<availablePackage>")
+        Console.println("  <package>")
+        Console.println("    <name></name>")
+        Console.println("    <version></version>")
+        Console.println("    <depends></depends>")
+        Console.println("    <description></description>")
+        Console.println("  </package>")
+        Console.println("<link></link>")
+        Console.println("</availablePackage>")
+        null  // return() here causes a compile error
 
       case List("-f", fname) =>  // COMPAT.  remove before long...
-	AvailablePackageUtil.fromXML(XML.load(fname))
+        AvailablePackageUtil.fromXML(XML.load(fname))
 
       case List(fname)  =>
-	AvailablePackageUtil.fromXML(XML.load(fname))
-      
+        try {
+          AvailablePackageUtil.fromXML(XML.load(fname))
+        } catch {
+          case ex: SAXParseException =>
+            if (new File(fname).exists()) {
+              Console.println("Invalid XML for a package description.")
+              Console.println("Expected is a valid .advert file.")
+              exit(2)
+            }
+            else
+              throw ex
+          case ex =>
+            throw ex
+        }
+
       case List("-i", arg) =>
-	try {
-	  AvailablePackageUtil.fromXML(XML.load(new StringReader(arg)))
-	} catch {
-	  case ex:FormatError =>
-	    if(new File(arg).exists()) {
-	      Console.println("Invalid XML for a package description.")
-	      Console.println("Did you mean to specify -f?")
-	      exit(2)
-	    } else {
-	      throw ex
-	    }
-	  case ex =>
-	    throw ex
-	}
+        try {
+          AvailablePackageUtil.fromXML(XML.load(new StringReader(arg)))
+        } catch {
+          case ex: FormatError =>
+            if (new File(arg).exists()) {
+              Console.println("Invalid XML for a package description.")
+              Console.println("Did you mean to specify -f?")
+              exit(2)
+            } else {
+              throw ex
+            }
+          case ex =>
+            throw ex
+        }
       
       case _ =>
         usageExit
     }
 
-    if (pack == null)
-      return()
-
-
+    if (pack == null) return
 
     // XXX this should do some sanity checks on the package:
     //  non-empty name, version, etc.
