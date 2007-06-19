54857e13e290373dccf132152f6218df5fad98fb
phaller <phaller@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-06-19 09:45:06 +0000

    Replaced BigInteger example for Java interaction by FrenchDate example. Added Scala source.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-documentation/trunk@12076 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

---
+++ src/tutorial/FrenchDate.scala
@@ -0,0 +1,11 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._
+
+object FrenchDate {
+  def main(args: Array[String]) {
+    val now = new Date
+    val df = getDateInstance(LONG, Locale.FRANCE)
+    println(df format now)
+  }
+}
