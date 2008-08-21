d9d077f3c631b38c24ef8fc2fa6b8abb04c8e92c
rytz <rytz@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-08-21 13:12:55 +0000

    removed ScalaTour from build as it depends on files of the old website. ScalaTour will be avaialable online uniquely.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-documentation/trunk@15875 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- bin/lbraces.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for file in $@; do
-  name=tour/`basename $file`;
-  #echo name=$name;
-  awk '/<src/||/<pre/{on=1};/<\/src>/||/<\/pre>/{off=1};{if(on){gsub("{","\\{");gsub("}","\\}")};if(off){on=0;off=0};print $0}' \
-      $file > $BUILDDIR/$name;
-done
