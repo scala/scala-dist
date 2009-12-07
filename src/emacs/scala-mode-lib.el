884a89bbc96976dadde2576eda0ec5ca67dd495f
cunei <cunei@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-12-07 11:59:42 +0000

    Updated copyright notices to 2010
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala-tool-support/trunk@20031 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/emacs/scala-mode-lib.el
+++ src/emacs/scala-mode-lib.el
@@ -10,7 +10,7 @@
 
 ;; SCALA LICENSE
 ;;  
-;; Copyright (c) 2002-2009 EPFL, Lausanne, unless otherwise specified.
+;; Copyright (c) 2002-2010 EPFL, Lausanne, unless otherwise specified.
 ;; All rights reserved.
 ;;  
 ;; This software was developed by the Programming Methods Laboratory of the
