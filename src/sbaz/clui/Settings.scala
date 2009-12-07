6c8a63beffdad77ad5169bd66a986e1af0a4ba9b
cunei <cunei@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-12-07 12:01:47 +0000

    Updated copyright notices to 2010
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/sbaz/trunk@20032 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/sbaz/clui/Settings.scala
+++ src/sbaz/clui/Settings.scala
@@ -1,5 +1,5 @@
 /* SBaz -- Scala Bazaar
- * Copyright 2005-2009 LAMP/EPFL
+ * Copyright 2005-2010 LAMP/EPFL
  * @author  Lex Spoon
  */
 
@@ -19,7 +19,7 @@ class Settings {
   val PRODUCT: String = "sbaz"
   val VERSION: String = sbaz.MyVersion.versionString
   val COPYRIGHT: String =
-    System.getProperty("scala.copyright", "(c) 2005-2009 LAMP/EPFL")
+    System.getProperty("scala.copyright", "(c) 2005-2010 LAMP/EPFL")
   val versionMsg = PRODUCT + " " + VERSION + " -- " + COPYRIGHT
 
   // the name of the directory that is being managed
