b73005ffb71e9a5b2f8959f97db66c5cedaf640d
Josh Suereth <joshua.suereth@gmail.com>
2012-03-30 16:12:18 -0400

    Fixed naming scheme on RPM so we can pull it into the release.

--- installer/project/build.scala
+++ installer/project/build.scala
@@ -208,6 +208,12 @@ object ScalaDistro extends Build with WindowsPackaging with Versioning {
     rpmVendor := "typesafe",
     rpmUrl := Some("http://github.com/scala/scala"),
     rpmLicense := Some("BSD"),
+    // This hack lets us ignore the RPM specific versioning junks.
+    packageBin in Rpm <<= (packageBin in Rpm, target, name, version) map { (p, t, n, v) =>
+      val rpm = t / (n + "-" + v + ".rpm")
+      IO.copyFile(p, rpm)
+      rpm
+    },
     
     // Debian Specific
     name in Debian := "scala",
