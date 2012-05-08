7aa5f9b559bbf231e682d77ec45597fc8884bac7
Josh Suereth <joshua.suereth@gmail.com>
2012-05-08 14:18:27 -0400

    Added hook to compile examples with the same scala distribution used to release.

--- project/build.scala
+++ project/build.scala
@@ -9,6 +9,8 @@ import collection.mutable.ArrayBuffer
 object ScalaDistBuild extends {
   override val root = Project("root", file(".")) settings(ScalaDistroFinder.allSettings:_*)
   override val scalaDistDir: TaskKey[File] = ScalaDistroFinder.scalaDistDir in root
-} with Build with ScalaInstallerBuild {
-  override def projects = Seq(root, installer)
+} with Build 
+  with ScalaInstallerBuild
+  with ExamplesBuild {
+  override def projects = Seq(root, examples, installer)
 }
