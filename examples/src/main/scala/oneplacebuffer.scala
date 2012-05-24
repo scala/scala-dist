f1b503d79e75b3bd7e900265184cb36041bd118b
Som Snytt <som.snytt@gmail.com>
2012-05-23 21:48:46 -0700

    OnePlaceBuffer uses Future, stops cleanly

--- examples/src/main/scala/oneplacebuffer.scala
+++ examples/src/main/scala/oneplacebuffer.scala
@@ -1,13 +1,14 @@
 package examples
 
-object oneplacebuffer {
+object onePlaceBuffer {
 
   import scala.actors.Actor._
-  import scala.concurrent.ops
+  import scala.concurrent._
 
   class OnePlaceBuffer {
     private case class Put(x: Int)
     private case object Get
+    private case object Stop
 
     private val m = actor {
       var buf: Option[Int] = None
@@ -20,45 +21,46 @@ object oneplacebuffer {
             val x = buf.get
             println("get "+x)
             buf = None; reply(x)
+          case Stop => exit()
         }
       }
     }
-    m.start()
 
     def write(x: Int) { m !? Put(x) }
 
     def read(): Int = (m !? Get).asInstanceOf[Int]
-  }
 
-  def kill(delay: Int) = new java.util.Timer().schedule(
-    new java.util.TimerTask {
-      override def run() {
-        println("[killed]")
-        sys exit 0
-      }
-    },
-    delay) // in milliseconds
+    def finish() { m ! Stop }
+  }
 
   def main(args: Array[String]) {
     val buf = new OnePlaceBuffer
     val random = new java.util.Random()
-
+    val sinker = new SyncVar[Boolean]
+    val MaxWait = 500L
+    def isDone = sinker.isSet && sinker.get(MaxWait).getOrElse(false)
+    def finish = sinker.put(true)
+    def kill(delay: Long) = new java.util.Timer().schedule(
+      new java.util.TimerTask {
+        override def run() { finish }
+      },
+      delay) // in milliseconds
     def producer(n: Int) {
+      if (isDone) { buf write -1; return }
       Thread.sleep(random nextInt 1000)
       buf write n
       producer(n + 1)
     }
-
     def consumer {
       Thread.sleep(random nextInt 1000)
       val n = buf.read()
+      if (n < 0) return
       consumer
     }
 
-    ops spawn producer(0)
-    ops spawn consumer
+    val maker = future { producer(0) }
+    maker onComplete { e => buf.finish() }
+    val taker = future { consumer }
     kill(10000)
   }
-
 }
-
