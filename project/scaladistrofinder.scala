d941e062c989e72f6d5ace593969b74f95a023bc
Josh Suereth <joshua.suereth@gmail.com>
2012-11-28 09:09:48 -0500

    Fix issue for 2.10.0-RC3

--- project/scaladistrofinder.scala
+++ project/scaladistrofinder.scala
@@ -119,7 +119,7 @@ object ScalaDistroFinder {
   def modules = Map(
     "lib/akka-actors.jar"            -> "https://oss.sonatype.org/content/repositories/releases/com/typesafe/akka/akka-actor_2.10.0-RC3/2.1.0-RC3/akka-actor_2.10.0-RC3-2.1.0-RC3.jar",
     "lib/typesafe-config.jar"        -> "https://oss.sonatype.org/content/repositories/releases/com/typesafe/config/1.0.0/config-1.0.0.jar",
-    "lib/scala-actors-migration.jar" -> "https://oss.sonatype.org/content/repositories/releases/org/scala-lang/scala-actors-migration_2.10.0-RC3/1.0.0-RC2/scala-actors-migration_2.10.0-RC3-1.0.0-RC3.jar"
+    "lib/scala-actors-migration.jar" -> "https://oss.sonatype.org/content/repositories/releases/org/scala-lang/scala-actors-migration_2.10.0-RC3/1.0.0-RC3/scala-actors-migration_2.10.0-RC3-1.0.0-RC3.jar"
   )
     
   def obtainModules(dir: File): Unit = {
