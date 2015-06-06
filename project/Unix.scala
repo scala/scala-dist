897057d28db96f2f27c740400066a4686292e567
Bennett Kanuka <bkanuka@gmail.com>
2015-06-06 16:40:46 -0400

    Explicitly requiring Java 6 or above

--- project/Unix.scala
+++ project/Unix.scala
@@ -87,7 +87,7 @@ object Unix {
 
     // Debian Specific
     name in Debian    := "scala",
-    debianPackageDependencies += "openjdk-7-jre-headless | java-runtime-headless",
+    debianPackageDependencies += "openjdk-7-jre-headless | java-runtime-headless (>= 1.6)",
     // debianPackageDependencies += "libjansi-java",
 
     linuxPackageMappings in Debian += (packageMapping(
