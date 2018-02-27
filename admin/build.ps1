0952eba2ffe00a4878382c04880a030301714c9a
Lukas Rytz <lukas.rytz@gmail.com>
2018-02-27 10:05:08 +0100

    AppVeyor script

---
+++ admin/build.ps1
@@ -0,0 +1,36 @@
+$ErrorActionPreference = "Stop"
+
+Function checkExit() {
+  if (-not $?) {
+    echo "Last command failed."
+    Exit 1
+  }
+}
+
+# oh boy. i don't (want to) fully understand, but executing commands and redirecting is difficult.
+#  - https://mnaoumov.wordpress.com/2015/01/11/execution-of-external-commands-in-powershell-done-right/
+#  - https://stackoverflow.com/a/35980675/248998
+# letting cmd do the redirect avoids confusing powershell.
+& cmd /c 'java -version' '2>&1'
+checkExit
+
+Function ensureVersion() {
+  $verPat="^[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)?$"
+  if ($env:version -notmatch $verPat) {
+    echo "Not a valid Scala version: '$env:version'"
+    Exit 1
+  }
+}
+
+if ($env:APPVEYOR_FORCED_BUILD -eq 'true') {
+  ensureVersion
+  if ($env:mode -eq 'release') {
+    echo "Running a release for $env:version"
+  } else {
+    echo "Unknown mode: '$env:mode'"
+  }
+} else {
+  # By default, test building the packages (but don't uplaod)
+  & sbt "-Dproject.version=2.12.0" "show s3Upload::mappings"
+  checkExit
+}
